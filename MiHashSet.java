5a162e591f3ac1c77f6c1024760522d1a098c609
paco <franmenendezpouso@gmail.com>
2016-03-14 12:32:18 +0100

    metodo Add

---
+++ MiHashSet.java
@@ -0,0 +1,99 @@
+
+/**
+ * Write a description of class MiHashSet here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MiHashSet
+{
+    // instance variables - replace the example below with your own
+    private int[] enteros;
+
+    /**
+     * Constructor for objects of class MiHashSet
+     */
+    public MiHashSet()
+    {
+        // initialise instance variables
+        enteros = new int[0];
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+     public void add(int valor)
+    {
+        int[] local;
+        local = enteros;
+        enteros = new int[local.length + 1];
+        for (int cont = 0; cont < local.length;cont++)
+        {
+            enteros[cont] = local[cont];
+        }
+        enteros[local.length] = valor;
+    }
+    
+     public void clear()
+    {
+        enteros = new int[0];
+    }
+    
+     public boolean contains(int elemento)
+    {
+        boolean contiene = false;
+        int cont = 0;
+        while(cont < enteros.length && !contiene)
+        {
+             if (enteros[cont] == elemento)
+             {
+                 contiene = true;
+             }
+             cont++;
+        }
+        return contiene;
+    }
+    
+     public boolean isEmpty()
+    {
+        return enteros.length == 0;
+    }
+    
+     public int remove(int elemento)
+    {
+        int borrado = -1;
+        if (elemento <= enteros.length && elemento >= 0)
+        {
+            int[] local;
+            local = enteros;
+            enteros = new int[local.length - 1];
+            for (int cont = 0; cont < enteros.length;cont++)
+            {
+                if (cont < elemento)
+                {
+                   enteros[cont] = local[cont];
+                }
+                else
+                {
+                    enteros[cont] = local[cont+1];
+                }
+            }
+            borrado = local[elemento];
+        }
+        return borrado;
+    }
+    
+    public int size()
+    {
+        return enteros.length;
+    }
+    
+    public String toString(){
+     String cadena ="";
+        
+     return cadena;
+    }
+}
