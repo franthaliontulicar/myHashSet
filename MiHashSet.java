aa0b218f628c2351513b3c3a443213844ff93de4
paco <franmenendezpouso@gmail.com>
2016-03-14 13:07:53 +0100

    metodos corregidos usando la clase ArrayListInt

--- MiHashSet.java
+++ MiHashSet.java
@@ -8,7 +8,7 @@
 public class MiHashSet
 {
     // instance variables - replace the example below with your own
-    private int[] enteros;
+    private ArrayListInt enteros;
 
     /**
      * Constructor for objects of class MiHashSet
@@ -16,7 +16,7 @@ public class MiHashSet
     public MiHashSet()
     {
         // initialise instance variables
-        enteros = new int[0];
+        enteros = new ArrayListInt();
     }
 
     /**
@@ -27,71 +27,48 @@ public class MiHashSet
      */
      public void add(int valor)
     {
-        int[] local;
-        local = enteros;
-        enteros = new int[local.length + 1];
-        for (int cont = 0; cont < local.length;cont++)
-        {
-            enteros[cont] = local[cont];
-        }
-        enteros[local.length] = valor;
+        enteros.add(valor);
     }
     
     public void clear()
     {
-        enteros = new int[0];
+        enteros = new ArrayListInt();
     }
     
      public boolean contains(int elemento)
     {
-        boolean contiene = false;
-        int cont = 0;
-        while(cont < enteros.length && !contiene)
-        {
-             if (enteros[cont] == elemento)
-             {
-                 contiene = true;
-             }
-             cont++;
-        }
-        return contiene;
+        return enteros.contains(elemento);
     }
     
      public boolean isEmpty()
     {
-        return enteros.length == 0;
+        return enteros.size() == 0;
     }
     
      public int remove(int elemento)
     {
         int borrado = -1;
-        if (elemento <= enteros.length && elemento >= 0)
+       
+        if(contains(elemento))
         {
-            int[] local;
-            local = enteros;
-            enteros = new int[local.length - 1];
-            for (int cont = 0; cont < enteros.length;cont++)
-            {
-                if (cont < elemento)
-                {
-                   enteros[cont] = local[cont];
-                }
-                else
-                {
-                    enteros[cont] = local[cont+1];
-                }
-            }
-            borrado = local[elemento];
+            int index = enteros.indexOf(elemento);  
+            enteros.remove(index);
+            borrado = enteros.remove(index);
         }
+       
         return borrado;
     }
     
     public int size()
     {
-        return enteros.length;
+        return enteros.size();
     }
     
     public String toString(){
-        return enteros.length+"";
+        return enteros.size()+"";
+    }
+    
+    public boolean equals(MiHashSet otroConjunto){
+        boolean iguales = false;
     }
 }
